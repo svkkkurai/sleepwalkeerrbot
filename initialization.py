d405aff9d2385897c988fa9e27ee1484c299c42f
svkkkurai <sakurai.pnwz@gmail.com>
2025-02-04 00:56:25 +0200

    🛠️ Обновление до версии 1.07

--- initialization.py
+++ initialization.py
@@ -7,11 +7,11 @@ def createcfg():
 
     if not os.path.exists(file_path):
         config_content = '''\
-    TOKEN = ""  # Bot token
-    CHANNEL_ID = ""  # Id of your channel
-    MODER_CHAT_ID = ""  # Id of moderator chat
-    ADMIN_ID =  # Admin's ID
-    SENIOR_ADMIN =  # Your own ID. Needs for high permissions
+TOKEN = ""  # Bot token
+CHANNEL_ID = ""  # Id of your channel
+MODER_CHAT_ID = ""  # Id of moderator chat
+ADMIN_ID =  # Admin's ID
+SENIOR_ADMIN =  # Your own ID. Needs for high permissions
     '''
 
         with open(file_path, "w", encoding="utf-8") as file:
