810afae69593810742425e154dbebc218c41b60a
svkkkurai <sakurai.pnwz@gmail.com>
2025-02-03 23:33:11 +0200

    Убраны пробелы, причиняющие ошибки

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-    TOKEN = ""  # Bot token
-    CHANNEL_ID = ""  # Id of your channel
-    MODER_CHAT_ID = ""  # Id of moderator chat
-    ADMIN_ID =  # Admin's ID
-    SENIOR_ADMIN =  # Your own ID. Needs for high permissions
+TOKEN = ""  # Bot token
+CHANNEL_ID = ""  # Id of your channel
+MODER_CHAT_ID = ""  # Id of moderator chat
+ADMIN_ID =  # Admin's ID
+SENIOR_ADMIN =  # Your own ID. Needs for high permissions
     
